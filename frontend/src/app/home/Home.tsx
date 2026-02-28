2ef30e8034d3b33f59a84b714b024901b251e4a8
ADI-2707 <aditya21singh2707@gmail.com>
2026-02-28 11:57:42 +0530

    implemented co-located Home component architecture with strict CSS module discipline

---
+++ frontend/src/app/home/Home.tsx
@@ -0,0 +1,9 @@
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1>VSM Venture Control Systems Pvt. Ltd.</h1>
+    </main>
+  );
+}
