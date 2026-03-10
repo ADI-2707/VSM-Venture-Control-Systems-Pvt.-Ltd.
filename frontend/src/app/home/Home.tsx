fd5b299ede56c3f76f8309ebcac59604c2a05a1f
ADI-2707 <aditya21singh2707@gmail.com>
2026-03-10 12:04:13 +0530

    integrated projects section into home page

--- frontend/src/app/home/Home.tsx
+++ frontend/src/app/home/Home.tsx
@@ -1,10 +1,12 @@
 import styles from "./Home.module.css";
 import Hero from "@/sections/hero/Hero";
+import Projects from "@/sections/projects/Projects/Projects";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Hero />
+      <Projects />
 
       <section className={styles.textSection}>
         <h1>VSM Venture Control Systems Pvt. Ltd.</h1>
