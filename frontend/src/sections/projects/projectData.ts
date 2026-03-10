9077f2158f386a5ea618ec6f82a8417395e1b20d
ADI-2707 <aditya21singh2707@gmail.com>
2026-03-10 12:07:14 +0530

    image format matched for now

--- frontend/src/sections/projects/projectData.ts
+++ frontend/src/sections/projects/projectData.ts
@@ -9,28 +9,28 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    image: "/images/projects/hot-strip.jpg",
+    image: "/images/projects/hot-strip.jfif",
     application: "Hot Strip Mill",
     customer: "Hira Group",
     location: "Raipur Chhattisgarh"
   },
   {
     id: 2,
-    image: "/images/projects/pickling-line.jpg",
+    image: "/images/projects/pickling-line.jfif",
     application: "Pickling Line",
     customer: "JSOL",
     location: "Orissa"
   },
   {
     id: 3,
-    image: "/images/projects/skin-pass.jpg",
+    image: "/images/projects/skin-pass.jfif",
     application: "Skin Pass Mill",
     customer: "AMNS",
     location: "Hazira Gujarat"
   },
   {
     id: 4,
-    image: "/images/projects/paper-machine.jpg",
+    image: "/images/projects/paper-machine.jfif",
     application: "Paper Machine",
     customer: "Magnum Paper",
     location: "Sahibabad UP"
