a51c2ec3533e1fbbe93bf7dc1c5ac3d833a88af3
ADI-2707 <aditya21singh2707@gmail.com>
2026-04-01 17:39:08 +0530

    feat(products-ui): applied Reveal animation to products layout for smoother entry

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -1,5 +1,11 @@
 import ProductsLayout from "@/sections/products/ProductsLayout/ProductsLayout";
+import Reveal from "@/components/Reveal/Reveal";
 
 export default function ProductsPage() {
-  return <ProductsLayout />;
+  return (
+    
+    <Reveal>
+      <ProductsLayout />
+    </Reveal>
+  );
 }
