335ff4dc5ec11cb115e213bd0be333ee978b4ccf
ADI-2707 <aditya21singh2707@gmail.com>
2026-04-29 10:22:19 +0530

    feat(users): add get all users api endpoint

--- backend/app/services/user_services.py
+++ backend/app/services/user_services.py
@@ -29,3 +29,6 @@ def create_user(db: Session, user_data):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+def get_all_users(db: Session):
+    return db.query(User).all()
